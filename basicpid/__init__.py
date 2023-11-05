7ebf87891334dab977d497e7dcf3478c77fb29dd
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-11-05 18:49:28 -0500

    Update __init__.py

--- basicpid/__init__.py
+++ basicpid/__init__.py
@@ -1,17 +1,21 @@
 
 """
-basic-pid - PID Controller for discretized time
+basic-pid - PID Controller for discretized time regulators
 """
 
 # flit uses __version__ as one source version 
-# note: pep versions like > 'x.y.<number>[a-c]<digit>'
-# for example: 0.01.08d doesnt fly building from pyproject.toml
+# setup.cfg uses version = attr: robopid.__version__ 
+# note: pypi pep versions like this form > 'x.y.<number>[a-c]<digit>'
+# but 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = 'v0.02.04'
+
+__version__ = 'v0.02.05a'
 
 # import acts to export 
 
 from .pid import BasicPid
 from .pid import BasicPid as Pid
+from .pid import BasicPid as BasicPID
+
 
 
