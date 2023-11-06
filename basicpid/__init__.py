a4e29cedfddaf6f118516948fd8f3f9853ed182a
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-11-06 16:35:18 -0500

    Update __init__.py

--- basicpid/__init__.py
+++ basicpid/__init__.py
@@ -9,7 +9,7 @@ basic-pid - PID Controller for discretized time regulators
 # but 0.01.08d doesnt fly building from pyproject.toml
 
 
-__version__ = 'v0.02.05a'
+__version__ = 'v0.02.05b'
 
 # import acts to export 
 
