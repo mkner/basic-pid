dfc7dfeb9190c86427392358c8ef1e80a8b9cb0a
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-11-16 16:51:43 -0500

    Update pid.py

--- basicpid/pid.py
+++ basicpid/pid.py
@@ -20,7 +20,7 @@ class BasicPid(object):
        self._name = "BasicPid"
        self._desc = "Easy to use classic PID controller"
        self._vers = "v0.02.07" ## "v0.02.03 <-- ... v0.01.11b1"  #"v0.01.11b1 from dev v0.01.07b"
-       self._about = "BasicPid is an easy to use classic PID controller designed to be used in discretized time regulators"
+       self._about = "BasicPid is an easy to use PID controller designed for discrete-time regulators"
 
        #PID
        self._Kp = 1.0
